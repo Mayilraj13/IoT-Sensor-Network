b791d44a772923ff26b58193ee33e7ef8795229c
MAYILRAJ R <163078420+Blackboy13@users.noreply.github.com>
2025-02-21 22:30:02 +0530

    Update Humidity_Sensing_Using_Arduino.ino

--- Humidity Sensing Using Arduino/Humidity_Sensing_Using_Arduino.ino
+++ Humidity Sensing Using Arduino/Humidity_Sensing_Using_Arduino.ino
@@ -1,8 +1,6 @@
 #include "DHT.h"
-
-#define DHTPIN 8     // Digital pin connected to DHT11
-#define DHTTYPE DHT11   // DHT 11
-
+#define DHTPIN 8     
+#define DHTTYPE DHT11   
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
@@ -14,20 +12,14 @@ void setup() {
 
 void loop() {
   float h = dht.readHumidity();
-  float t = dht.readTemperature();// To Read temperature as Fahrenheit, add (isFahrenheit = true)
+  float t = dht.readTemperature();
   float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+ if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println(F("Cannot read from DHT Sensor attached..."));
     return;
   }
-
-  // Compute heat index in Fahrenheit (the default)
-  float heat_index_f = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float heat_index_c = dht.computeHeatIndex(t, h, false);
-
+ float heat_index_f = dht.computeHeatIndex(f, h);
+ float heat_index_c = dht.computeHeatIndex(t, h, false);
   Serial.print(F("Humidity: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
